353c8f365967e40aff6d68fee2dbf91581041811
aeliseeva3 <a.eliseeva3@g.nsu.ru>
2026-05-16 11:57:13 +0700

    Update 13-8.py

--- 13-8.py
+++ 13-8.py
@@ -6,7 +6,7 @@ numbers = sorted(set(map(int, input().split())))
 all_subsets = []
 for num in range(len(numbers) + 1):
     for combo in combinations(numbers, num):
-        all_subsets.append(list(combo))
+        all_subsets.append(combo)
 
 for subset in all_subsets:
     print('{' + ', '.join(map(str, subset)) + '}')
